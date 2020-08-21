2ec8d9c1ba5d973708156abcbd17efc1365f5792
el-sanches <nikeair309@gmail.com>
2020-08-22 02:00:13 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,33 @@
-/*=require ./includes/blocks/*.js*/
 "use strict";
+
+/*=require ./includes/blocks/*.js*/
+var $bannerSliderEl = $('[data-banner-slide]');
+var bannerSlider = new Swiper($bannerSliderEl, {
+  slidesPerView: 1,
+  resistanceRatio: 0,
+  pagination: {
+    el: '[data-banner-slider-pag]',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '[data-banner-slider-next]',
+    prevEl: '[data-banner-slider-prev]'
+  }
+});
+var $projectSliderEl = $('[data-project-slider]');
+$projectSliderEl.each(function (index, el) {
+  var $sliderEl = $(el);
+  new Swiper($sliderEl, {
+    slidesPerView: 1,
+    resistanceRatio: 0,
+    pagination: {
+      el: $sliderEl.parent().find('[data-project-slider-pag]'),
+      clickable: true
+    },
+    navigation: {
+      nextEl: $sliderEl.find('[data-project-slider-next]'),
+      prevEl: $sliderEl.find('[data-project-slider-prev]')
+    }
+  });
+});
 //# sourceMappingURL=common.js.map
--- js/development.js
+++ js/development.js
@@ -1,15 +1,2 @@
 "use strict";
-
-var $projectSliderEl = $('[data-project-slider]');
-var projectSlider = new Swiper($projectSliderEl, {
-  slidesPerView: 1,
-  resistanceRatio: 0,
-  pagination: {
-    el: '[data-project-slider-pag]'
-  },
-  navigation: {
-    nextEl: '[data-project-slider-next]',
-    prevEl: '[data-project-slider-prev]'
-  }
-});
 //# sourceMappingURL=development.js.map
